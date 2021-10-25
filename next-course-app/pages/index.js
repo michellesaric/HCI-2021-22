78b29cb3f433d3f715d491d0abe2b15d08d3dbf6
„michellesaric“ <msaric03@fesb.hr>
2021-10-25 18:45:13 +0200

    I messed up my code on purpose hehe

--- next-course-app/pages/index.js
+++ next-course-app/pages/index.js
@@ -12,19 +12,17 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+                 <h1 className={styles.title}>
+Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
+<p className={styles.description}>
+                             Get started by editing{' '}
+                   <code className={styles.code}>pages/index.js</code>
+           </p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+                                          <div className={styles.grid}>
+<a href="https://nextjs.org/docs" className={styles.card}>
+                      <h2>Documentation &rarr;</h2><p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
