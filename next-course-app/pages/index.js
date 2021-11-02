3fbf7972572c9ae9ecac48361865cb6cd35969be
„michellesaric“ <msaric03@fesb.hr>
2021-11-02 21:23:38 +0100

    Sent all the work done so far from the computer in the laboratory, first four exercises

--- next-course-app/pages/index.js
+++ next-course-app/pages/index.js
@@ -12,17 +12,21 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-                 <h1 className={styles.title}>
-Welcome to <a href="https://nextjs.org">Next.js!</a>
-            </h1>
-<p className={styles.description}>
-                             Get started by editing{' '}
-                   <code className={styles.code}>pages/index.js</code>
-           </p>
+<h1
+  className={styles.title}>
+          Welcome   to
+          <a href="https://nextjs.org">Korisnička sučelja!</a>
+  </h1>
 
-                                          <div className={styles.grid}>
-<a href="https://nextjs.org/docs" className={styles.card}>
-                      <h2>Documentation &rarr;</h2><p>Find in-depth information about Next.js features and API.</p>
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
